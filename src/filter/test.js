a2a540ef2caf5661b615520aa5bc1f76c3694c2c
Jalal azimi <jalalazimi@gmail.com>
2020-08-17 18:35:09 +0100

    test: write test for filter

--- src/filter/test.js
+++ src/filter/test.js
@@ -1,16 +1,12 @@
 // @flow
 /* eslint-env mocha */
 
-// import assert from 'power-assert'
-// import filter from '.'
+import assert from 'power-assert'
+import filter from '.'
 
-describe('get', function() {
-  // it('get should get string keyed property values', function() {
-  //   const object = { a: 1 }
-  //   assert.strictEqual(filter(object, 'a'), 1)
-  // })
-  //
-  // it('get should return the default value when `path` is empty', function() {
-  //   assert.strictEqual(filter({}, [], 'a'), 'a')
-  // })
+describe('filter', function() {
+  it('get should filter undefined items', function() {
+    const array = [1, 2, undefined, 3, undefined]
+    assert.deepStrictEqual(filter(array, Boolean), [1, 2, 3])
+  })
 })
