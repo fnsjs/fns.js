e7d2014dd4de4f9d7e5968d4261d450bd5471d37
Jalal azimi <jalalazimi@gmail.com>
2020-08-18 23:17:44 +0100

    feat: upgrade nvm

--- scripts/test/node.sh
+++ scripts/test/node.sh
@@ -7,11 +7,10 @@
 export PATH="$(yarn bin):$PATH"
 
 # Update and source nvm
-curl -o ~/.nvm/nvm.sh https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/nvm.sh
+curl -o ~/.nvm/nvm.sh https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/nvm.sh
 source ~/.nvm/nvm.sh
 
-for version in 8 9 10 11 12
-do
+for version in 8 9 10 11 12; do
   echo "Running tests using Node.js $version"
   nvm install $version
   npm rebuild
